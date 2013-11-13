83e9148be2da3cbcd47b5b1b26ad1c6cbd6b6395
Francesca Castelli <franci.castelli@gmail.com>
2013-11-13 14:30:44 -0500

    php mysql hw

---
+++ 09-PHP_MySQL/code/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Fortune Cookie Monster</title>
+		<link rel="stylesheet" href="css/style.css"> 
+		 
+	</head>
+	
+	<body>
+		
+		
+	<?php
+		
+		
+		
+		
+	</body>
+</html>
